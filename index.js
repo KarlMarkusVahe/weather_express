0b4b5963dd5d43b6c7b56470e5e99a144c43e7c2
KarlMarkusVahe <113974010+KarlMarkusVahe@users.noreply.github.com>
2023-01-31 10:44:50 +0200

    Created form request responce with API data

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
+const {response} = require("express");
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.set('view engine', 'ejs')
 app.set('set', path.join(__dirname, 'views'))
@@ -14,6 +17,24 @@ app.get('/', (req, res) => {
         .then((response) => {
             return response.json()
         })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
+})
+
+app.post('/', function (req, res) {
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((response) => {
+        return response.json()
+    })
         .then((data) => {
             let description = data.weather[0].description
             let city = data.name
